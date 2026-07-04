c6d84d1bddc701c8d3c248bed6b331ab959c0586
arunbelursan1995@gmail.com <arunbelursan1995@gmail.com>
2026-07-04 10:55:22 +0200

    changing location west US

--- azure/main.tf
+++ azure/main.tf
@@ -7,7 +7,7 @@ variable "prefix" {
 variable "location" {
   description = "Azure region for all resources."
   type        = string
-  default     = "eastus"
+  default     = "westus"
 }
 
 variable "admin_username" {
